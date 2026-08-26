8b54c75ae9d8501d3312add7b94b8e8f18889926
Subodh106 <subodhloves106@gmail.com>
2026-08-26 13:20:43 +0545

    feat: add QueryProvider and ThemeProvider components
    - Implemented QueryProvider to manage React Query client state.
    - Created ThemeProvider to wrap the application with Next Themes provider.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Instrument_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/providers/theme-provider";
+import Queryprovider from "@/providers/query-provider";
+
 
 const instrumentSansHeading = Instrument_Sans({subsets:['latin'],variable:'--font-heading'});
 
@@ -23,10 +26,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, instrumentSansHeading.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Queryprovider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+            </Queryprovider>
+      </body>
     </html>
   );
 }
---
+++ frontend/providers/query-provider.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React, { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const Queryprovider = ({children}:{children : React.ReactNode}) => {
+  const [query , setQuery] = useState(() => new QueryClient());
+
+  return <QueryClientProvider client={query}>{children}</QueryClientProvider>
+}
+
+export default Queryprovider
