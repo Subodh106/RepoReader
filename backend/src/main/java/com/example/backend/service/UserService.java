0eb9471c9bf9587e34f238317c14a1e1f9c2a5b6
Subodh106 <subodhloves106@gmail.com>
2026-08-26 16:53:49 +0545

    fix: change parameter type of findByGithubId method from UUID to Long

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -5,9 +5,13 @@ import com.example.backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
+
+import static org.springframework.expression.common.ExpressionUtils.toLong;
 
 @Service
 @RequiredArgsConstructor
@@ -16,11 +20,28 @@ public class UserService {
     public final TextEncryptor textEncryptor;
 
     @Transactional
-    public User requiredById(UUID id){
+    public User requiredById(Long id){
         return userRepository.findByGithubId(id).orElseThrow(()->new IllegalArgumentException("User not foud"));
     }
     public String decryptAccessToken(User user){
         return textEncryptor.decrypt(user.getAccessToken());
     }
+
+    public User upsertFromGithub(Map<String , Object> attribute, String accessToken, String scopes) throws OAuth2AuthenticationException {
+    Long githubId = (Long) attribute.get("id");
+    String login = String.valueOf(attribute.get("login"));
+    String name = attribute.get("name") !=null ? String.valueOf(attribute.get("name")):login;
+    String avatarUrl = attribute.get("avatar_url")!=null ? String.valueOf(attribute.get("avatar_url")):null;
+    String encryptedToken = textEncryptor.encrypt(accessToken);
+    User user = userRepository.findByGithubId(githubId).orElseGet(User::new);
+    user.setGithubID(githubId);
+    user.setGithubUsername(login);
+    user.setUsername(name);
+    user.setAvatarUrl(avatarUrl);
+    user.setAccessToken(encryptedToken);
+    user.setTokenScope(scopes);
+    return user;
+    }
+
 
 }
