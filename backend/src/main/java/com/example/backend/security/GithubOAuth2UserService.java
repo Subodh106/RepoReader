c74d2c83109a0271fd0392c6d963c373dfa9b1c5
Subodh106 <subodhloves106@gmail.com>
2026-08-26 16:54:13 +0545

    feat: implement GithubOAuth2UserService with user loading functionality

--- backend/src/main/java/com/example/backend/security/GithubOAuth2UserService.java
+++ backend/src/main/java/com/example/backend/security/GithubOAuth2UserService.java
@@ -1,4 +1,31 @@
 package com.example.backend.security;
 
-public class GithubOAuth2UserService {
+import com.example.backend.entity.User;
+import com.example.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class GithubOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserService userService;
+    private final DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+
+    @Override
+    public AppUserPrinciple loadUser(@NonNull OAuth2UserRequest oAuth2UserRequest)  throws OAuth2AuthenticationException{
+        OAuth2User githubUser = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+        String accessToken = oAuth2UserRequest.getAccessToken().toString();
+        String scopes = String.join(",",oAuth2UserRequest.getAccessToken().getScopes());
+        User user = userService.upsertFromGithub(Objects.requireNonNull(githubUser.getAttribute(githubUser.getName())),accessToken,scopes);
+        return new AppUserPrinciple(user,githubUser.getAttribute(githubUser.getName()));
+
+    }
 }
