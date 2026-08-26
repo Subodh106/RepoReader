6349bcd9feebd7a6f5e53fe60b5ae9d2c1e962f8
Subodh106 <subodhloves106@gmail.com>
2026-08-26 16:54:41 +0545

    feat: refactor AppUserPrinciple class to include user attributes and authorities

--- backend/src/main/java/com/example/backend/security/AppUserPrinciple.java
+++ backend/src/main/java/com/example/backend/security/AppUserPrinciple.java
@@ -1,4 +1,42 @@
 package com.example.backend.security;
 
-public class AppUserPrincipal {
+import com.example.backend.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class AppUserPrinciple {
+    public  final User user;
+    public final Map<String , Object> attribute;
+
+    public UUID getId(){
+        return user.getId();
+    }
+    @Override
+    public Map<String,Object> getAttribute(){
+        return attribute;
+    }
+    @Override
+    public Collection<?extends GrantedAuthority> getAuthorities(){
+        return AuthorityUtils.createAuthorityList("ROLE_USER");
+    }
+    @Override
+    public String getName(){
+        return user.getId().toString();
+    }
+    @Override
+    public String getName(){
+        return user.getId().toString();
+    }
 }
