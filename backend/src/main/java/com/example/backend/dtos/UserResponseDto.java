50c411494dce1809e5df0de5c863b2a531ff3c17
Subodh106 <subodhloves106@gmail.com>
2026-08-27 14:36:45 +0545

    feat: create UserResponseDto class for user data transfer

---
+++ backend/src/main/java/com/example/backend/dtos/UserResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserResponseDto {
+    private UUID id;
+    private Long githubId;
+    private String githubUsername;
+    private String displayName;
+    private String avatarUrl;
+
+}
