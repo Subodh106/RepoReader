2b113cb95452e8a4b305051bb6e0b61cab3f0753
Subodh106 <subodhloves106@gmail.com>
2026-08-31 22:32:02 +0545

    feat: add findById method to UserRepository for UUID user retrieval

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.example.backend.repository;
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.lang.ScopedValue;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByGithubId(Long githubId );
+
+    Optional<User> findById(UUID userId);
 }
