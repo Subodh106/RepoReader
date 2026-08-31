0b62661f015de85a0219c57d8a0a4aa319406c78
Subodh106 <subodhloves106@gmail.com>
2026-08-31 22:31:53 +0545

    feat: extend RepoRepository interface with JPA support and additional query methods

--- backend/src/main/java/com/example/backend/repository/RepoRepository.java
+++ backend/src/main/java/com/example/backend/repository/RepoRepository.java
@@ -1,4 +1,17 @@
 package com.example.backend.repository;
 
-public interface RepoRepository {
+import com.example.backend.entity.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RepoRepository extends JpaRepository<Repository, UUID> {
+    List<Repository> findUserIdOrderByFullNameAsc(UUID userID);
+
+    Optional<Repository> findIdAndUserId(UUID repoId , UUID userId);
+
+    Optional<Repository> findByUserIdAndGithubRepoId(UUID userId , Long githubRepoId);
 }
+
